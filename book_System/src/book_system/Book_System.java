955ef4f41f761db6af317e607f8382923b015be6
XuantongLu <luxuantong1994@gmail.com>
2017-04-22 14:56:51 +0200

    1234

--- book_System/src/book_system/Book_System.java
+++ book_System/src/book_system/Book_System.java
@@ -18,6 +18,7 @@ public class Book_System {
         // TODO code application logic here
         System.out.println("Test");
         System.out.println("asd");
+        System.out.println("1234");
     }
     
 }
